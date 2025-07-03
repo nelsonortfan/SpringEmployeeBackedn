d94dc5afc8f7b78978dd67c00facf6c11feed840
nelsonortfan <n.ortizf@uniandes.edu.co>
2025-07-03 16:13:24 -0500

    Agregando respuesta personalizada errores y excepciones

---
+++ src/main/java/com/example/backend/demoReact/dto/ErrorResponseDto.java
@@ -0,0 +1,22 @@
+package com.example.backend.demoReact.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponseDto {
+
+    private String apiPath;
+
+    private HttpStatus errorCode;
+
+    private String errorMessage;
+
+    private LocalDateTime errorTime;
+
+
+}
---
+++ src/main/java/com/example/backend/demoReact/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.example.backend.demoReact.exception;
+
+import com.example.backend.demoReact.dto.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handledResourcerNotFoundException(ResourceNotFoundException exception,
+                                                                              WebRequest webRequest){
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.NOT_FOUND,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
+    }
+
+
+}
