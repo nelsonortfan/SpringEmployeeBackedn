bc9964dbe14a7b957f4b7b42d1d4c5d0686a6df6
nelsonortfan <n.ortizf@uniandes.edu.co>
2025-07-08 11:13:59 -0500

    test exception not found con Id inexistente

--- src/test/java/com/example/backend/demoReact/controller/EmployeeControllerTest.java
+++ src/test/java/com/example/backend/demoReact/controller/EmployeeControllerTest.java
@@ -2,6 +2,7 @@ package com.example.backend.demoReact.controller;
 
 import com.example.backend.demoReact.dto.EmployeeDto;
 import com.example.backend.demoReact.entity.Employee;
+import com.example.backend.demoReact.exception.ResourceNotFoundException;
 import com.example.backend.demoReact.service.EmployeeService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -71,6 +72,16 @@ class EmployeeControllerTest {
                 .andExpect(jsonPath("$.id").value(1))
                 .andExpect(jsonPath("$.firstName").value("Mauricio"));
 
+    }
+
+    @Test
+    void getEmployeeById_NotFound() throws Exception {
+
+        when(employeeService.getEmployeeById(4L))
+                .thenThrow(new ResourceNotFoundException("Employee not found with id: 4"));
+
+        this.mockMvc.perform(get("/api/employees/4")).andDo(print())
+                .andExpect(status().isNotFound());
     }
 
     @Test
